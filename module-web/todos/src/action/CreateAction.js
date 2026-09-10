33afb1a20fba079f429c07241471c22c8fe238e5
hairi11 <mohd.khushairi@gmail.com>
2026-09-11 00:08:47 +0800

    Let Todo form own Select2 lifecycle

--- module-web/todos/src/action/CreateAction.js
+++ module-web/todos/src/action/CreateAction.js
@@ -1,7 +1,5 @@
 import TodoFormAction from '../TodoFormAction.js';
-import { initStatusSelect } from './todoActionBase.js';
 
 export function initCreate() {
-    initStatusSelect();
     new TodoFormAction('#todoForm', { mode: 'create' }).build();
 }
