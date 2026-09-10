64f83219beb598a63848b42fada447f290acfcf9
hairi11 <mohd.khushairi@gmail.com>
2026-09-10 18:49:21 +0800

    Expose flatpickr in shared vendor bootstrap

--- module-web/webapp/src/js/vendor.js
+++ module-web/webapp/src/js/vendor.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import 'datatables.net-bs5';
 import 'datatables.net-select-bs5';
+import flatpickr from 'flatpickr';
 import select2 from 'select2';
 
 window.jQuery = window.$ = $;
+window.flatpickr = flatpickr;
 select2(window, $);
