7b84b4a677efea462145d3ca9f5cacf6092ffb5d
hairi11 <mohd.khushairi@gmail.com>
2026-09-11 00:08:58 +0800

    Use Todo form Select2 wrapper on update

--- module-web/todos/src/action/UpdateAction.js
+++ module-web/todos/src/action/UpdateAction.js
@@ -1,9 +1,7 @@
 import TodoFormAction from '../TodoFormAction.js';
-import { initStatusSelect, requireId } from './todoActionBase.js';
+import { requireId } from './todoActionBase.js';
 
 export async function initUpdate() {
-    initStatusSelect();
-
     const action = new TodoFormAction('#todoForm', {
         mode: 'update',
         id: requireId()
