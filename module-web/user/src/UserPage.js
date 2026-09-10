d773d4668f671b0adc56c33da86a5dbb29df4596
hairi11 <mohd.khushairi@gmail.com>
2026-09-10 15:56:49 +0800

    Use Bootstrap DataTables integration for users

--- module-web/user/src/UserPage.js
+++ module-web/user/src/UserPage.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
-import 'datatables.net';
-import 'datatables.net-select';
+import 'datatables.net-bs5';
+import 'datatables.net-select-bs5';
 import Common from '@company/common-js-web';
 import UserFormAction from './UserFormAction.js';
 import UserService from './UserService.js';
