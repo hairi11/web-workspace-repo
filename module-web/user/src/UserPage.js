f7a0e702146b17ccea6c11e87176522e220df604
hairi11 <mohd.khushairi@gmail.com>
2026-09-11 16:33:21 +0800

    Enable automatic runtime tracing for user flow

--- module-web/user/src/UserPage.js
+++ module-web/user/src/UserPage.js
@@ -1,13 +1,27 @@
+import Common from '@company/common-js-web';
+import UserFormAction from './UserFormAction.js';
+import UserService from './UserService.js';
+import UserApi from './UserApi.js';
 import { initEnquiry } from './action/EnquiryAction.js';
 import { initCreate } from './action/CreateAction.js';
 import { initUpdate } from './action/UpdateAction.js';
 import { initView } from './action/ViewAction.js';
+import { traceClass, traceFunction, traceObject, traceStatic } from './Trace.js';
+
+const { Ajax, DataTableBuilder, Storage } = Common;
+
+traceClass(UserFormAction, true);
+traceClass(DataTableBuilder, true);
+traceClass(Storage, true);
+traceStatic(Ajax, 'Ajax');
+traceObject(UserService, 'UserService');
+traceObject(UserApi, 'UserApi');
 
 const actions = {
-    enquiry: initEnquiry,
-    create: initCreate,
-    update: initUpdate,
-    view: initView
+    enquiry: traceFunction(initEnquiry, 'EnquiryAction.initEnquiry'),
+    create: traceFunction(initCreate, 'CreateAction.initCreate'),
+    update: traceFunction(initUpdate, 'UpdateAction.initUpdate'),
+    view: traceFunction(initView, 'ViewAction.initView')
 };
 
 async function init() {
@@ -25,4 +39,4 @@ async function init() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', traceFunction(init, 'UserPage.init'));
