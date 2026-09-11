cca68a2664a5e8c54185949f4673ac527543648e
hairi11 <mohd.khushairi@gmail.com>
2026-09-11 12:45:53 +0800

    Initialize user create wizard steps

--- module-web/user/src/action/CreateAction.js
+++ module-web/user/src/action/CreateAction.js
@@ -1,5 +1,23 @@
 import UserFormAction from '../UserFormAction.js';
+import { clearDraft, getDraft } from './userActionBase.js';
 
 export function initCreate() {
-    new UserFormAction('#userForm', { mode: 'create' }).build();
+    const step = document.body.dataset.step || 'profile';
+    const params = new URLSearchParams(window.location.search);
+
+    if (step === 'profile' && params.get('resume') !== '1') {
+        clearDraft();
+    }
+
+    const action = new UserFormAction('#userForm', {
+        mode: 'create',
+        step: step
+    });
+
+    action.build();
+
+    const draft = getDraft();
+    if (draft && draft.mode === 'create') {
+        action.populate(draft.data);
+    }
 }
