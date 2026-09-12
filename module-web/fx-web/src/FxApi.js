6c19743b782b2c599387ea1ae410234a345e3e0b
hairi11 <mohd.khushairi@gmail.com>
2026-09-12 22:33:38 +0800

    Add FX API module

---
+++ module-web/fx-web/src/FxApi.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'http://localhost:8080/api';
+
+const FxApi = {
+    masters: BASE_URL + '/fx-masters',
+    save: BASE_URL + '/fx/save',
+    submit: BASE_URL + '/fx/submit',
+
+    masterById: function (id) {
+        return this.masters + '/' + encodeURIComponent(id);
+    },
+
+    transactionsByMasterId: function (masterId) {
+        return this.masterById(masterId) + '/transactions';
+    },
+
+    transactionById: function (id) {
+        return BASE_URL + '/fx-transactions/' + encodeURIComponent(id);
+    }
+};
+
+export default FxApi;
