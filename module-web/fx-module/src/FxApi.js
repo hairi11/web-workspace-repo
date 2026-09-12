cae791b14c9a2d7388b5e5c07a037a081804872a
hairi11 <mohd.khushairi@gmail.com>
2026-09-13 01:32:38 +0800

    Add FX reference endpoint

--- module-web/fx-module/src/FxApi.js
+++ module-web/fx-module/src/FxApi.js
@@ -2,6 +2,7 @@ const BASE_URL = 'http://localhost:8080/api';
 
 const FxApi = {
     enquiry: BASE_URL + '/fx/enquiry',
+    references: BASE_URL + '/fx/references',
     masters: BASE_URL + '/fx-masters',
     save: BASE_URL + '/fx/save',
     submit: BASE_URL + '/fx/submit',
