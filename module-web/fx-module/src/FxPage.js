116da351939fef4290ae372364ba877623a587f9
hairi11 <mohd.khushairi@gmail.com>
2026-09-13 16:43:57 +0800

    Wire FX transaction form page

--- module-web/fx-module/src/FxPage.js
+++ module-web/fx-module/src/FxPage.js
@@ -1,9 +1,11 @@
 import { initEnquiry } from './action/EnquiryAction.js';
 import { initCreate } from './action/CreateAction.js';
+import { initTransaction } from './action/TransactionAction.js';
 
 const actions = {
     enquiry: initEnquiry,
-    create: initCreate
+    create: initCreate,
+    transaction: initTransaction
 };
 
 async function init() {
