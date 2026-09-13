78be0378f936516dac316ac56cab0f5182fd2a2f
hairi11 <mohd.khushairi@gmail.com>
2026-09-13 23:26:22 +0800

    Use NavigationState for todo update

--- module-web/todos/src/action/UpdateAction.js
+++ module-web/todos/src/action/UpdateAction.js
@@ -1,10 +1,20 @@
+import Common from '@company/common-js-web';
 import TodoFormAction from '../TodoFormAction.js';
-import { requireId } from './todoActionBase.js';
+
+const { NavigationState, Toast } = Common;
 
 export async function initUpdate() {
+    const navigation = NavigationState.consume();
+    const id = navigation && navigation.id;
+
+    if (!id) {
+        Toast.error('Todo id is required.');
+        throw new Error('Missing todo id.');
+    }
+
     const action = new TodoFormAction('#todoForm', {
         mode: 'update',
-        id: requireId()
+        id: id
     });
 
     await action.loadStatusOptions();
