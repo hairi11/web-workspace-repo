606d6ba010988de7e51de4b0ab0bef5fdde88e8e
hairi11 <mohd.khushairi@gmail.com>
2026-09-13 22:46:53 +0800

    Route FX pages through common PageRouter

--- module-web/fx-module/src/FxPage.js
+++ module-web/fx-module/src/FxPage.js
@@ -1,24 +1,12 @@
+import Common from '@company/common-js-web';
 import { initEnquiry } from './action/EnquiryAction.js';
 import { initCreate } from './action/CreateAction.js';
 import { initTransaction } from './action/TransactionAction.js';
 
-const actions = {
-    enquiry: initEnquiry,
-    create: initCreate,
-    transaction: initTransaction
-};
+const { PageRouter } = Common;
 
-async function init() {
-    const page = document.body.dataset.page;
-
-    if (!page) {
-        return;
-    }
-
-    const action = actions[page];
-    if (action) {
-        await action();
-    }
-}
-
-document.addEventListener('DOMContentLoaded', init);
+new PageRouter()
+    .route('enquiry', initEnquiry)
+    .route('create', initCreate)
+    .route('transaction', initTransaction)
+    .start();
