b34b7a1597e01652b1ed87e2c103295050b560e6
hairi11 <mohd.khushairi@gmail.com>
2026-09-13 23:23:55 +0800

    Refactor todo module to PageRouter

--- module-web/todos/src/TodoPage.js
+++ module-web/todos/src/TodoPage.js
@@ -1,28 +1,14 @@
+import Common from '@company/common-js-web';
 import { initEnquiry } from './action/EnquiryAction.js';
 import { initCreate } from './action/CreateAction.js';
 import { initUpdate } from './action/UpdateAction.js';
 import { initView } from './action/ViewAction.js';
 
-const actions = {
-    enquiry: initEnquiry,
-    create: initCreate,
-    update: initUpdate,
-    view: initView
-};
+const { PageRouter } = Common;
 
-async function init() {
-    try {
-        const page = document.body.dataset.page;
-        const action = actions[page];
-
-        if (!action) {
-            throw new Error('Unsupported todo page: ' + page);
-        }
-
-        await action();
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-document.addEventListener('DOMContentLoaded', init);
+new PageRouter()
+    .route('enquiry', initEnquiry)
+    .route('create', initCreate)
+    .route('update', initUpdate)
+    .route('view', initView)
+    .start();
