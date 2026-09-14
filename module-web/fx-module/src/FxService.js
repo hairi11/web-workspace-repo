80a0ed3169530916c1a419a165eb81f59b2056d5
hairi11 <mohd.khushairi@gmail.com>
2026-09-14 15:46:24 +0800

    Map FX REST payloads between snake and camel case

--- module-web/fx-module/src/FxService.js
+++ module-web/fx-module/src/FxService.js
@@ -1,10 +1,10 @@
 import Common from '@company/common-js-web';
 import FxApi from './FxApi.js';
 
-const { Ajax } = Common;
+const { Ajax, CaseUtil } = Common;
 
 function responseData(response) {
-    return response ? response.data : null;
+    return response ? CaseUtil.toCamelKeys(response.data) : null;
 }
 
 function responseObject(response) {
@@ -15,6 +15,13 @@ function responseObject(response) {
 function responseArray(response) {
     const data = responseData(response);
     return Array.isArray(data) ? data : [];
+}
+
+function post(url, data) {
+    return Ajax.post(url, CaseUtil.toSnakeKeys(data)).then((response) => {
+        if (response) response.data = CaseUtil.toCamelKeys(response.data);
+        return response;
+    });
 }
 
 const FxService = {
@@ -70,19 +77,19 @@ const FxService = {
     },
 
     updateTransaction: function (id, data) {
-        return Ajax.post(FxApi.transactionById(id), data);
+        return post(FxApi.transactionById(id), data);
     },
 
     deleteTransaction: function (id) {
-        return Ajax.post(FxApi.transactionById(id) + '/delete', {});
+        return post(FxApi.transactionById(id) + '/delete', {});
     },
 
     save: function (data) {
-        return Ajax.post(FxApi.save, data);
+        return post(FxApi.save, data);
     },
 
     submit: function (data) {
-        return Ajax.post(FxApi.submit, data);
+        return post(FxApi.submit, data);
     }
 };
 
