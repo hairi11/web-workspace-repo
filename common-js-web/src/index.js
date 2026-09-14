6df9e41a670c709f25a1bda3bebdb217b886208c
hairi11 <mohd.khushairi@gmail.com>
2026-09-14 16:00:13 +0800

    Export REST case interceptor

--- common-js-web/src/index.js
+++ common-js-web/src/index.js
@@ -4,6 +4,7 @@ module.exports = {
     Ajax: require('./ajax/Ajax'),
     AjaxError: require('./ajax/AjaxError'),
     AjaxResponse: require('./ajax/AjaxResponse'),
+    CaseInterceptor: require('./ajax/CaseInterceptor'),
     CaseUtil: require('./util/CaseUtil'),
     ConfirmDialog: require('./form/dialog/ConfirmDialog'),
     DatePicker: require('./datepicker/DatePicker'),
