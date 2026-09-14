5c9a809d1989df915a5c037afbadcc942e7b934c
hairi11 <mohd.khushairi@gmail.com>
2026-09-14 22:33:11 +0800

    Route FX master page

--- module-web/fx-module/src/FxPage.js
+++ module-web/fx-module/src/FxPage.js
@@ -1,12 +1,12 @@
 import Common from '@company/common-js-web';
 import { initEnquiry } from './action/EnquiryAction.js';
-import { initCreate } from './action/CreateAction.js';
+import { initMaster } from './action/MasterAction.js';
 import { initTransaction } from './action/TransactionAction.js';
 
 const { PageRouter } = Common;
 
 new PageRouter()
     .route('enquiry', initEnquiry)
-    .route('create', initCreate)
+    .route('master', initMaster)
     .route('transaction', initTransaction)
     .start();
