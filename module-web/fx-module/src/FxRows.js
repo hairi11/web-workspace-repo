9d29c31bd46b1b0071032490312997c1f46d4f62
hairi11 <mohd.khushairi@gmail.com>
2026-09-15 10:45:56 +0800

    Improve FX rows key generation for Sonar compliance

--- module-web/fx-module/src/FxRows.js
+++ module-web/fx-module/src/FxRows.js
@@ -6,7 +6,7 @@ const ROWS_KEY_PREFIX = 'fx.rows.';
 const storage = new Storage(window.sessionStorage);
 
 function newRowsKey() {
-    return 'new-' + Date.now().toString(36) + '-' + Math.random().toString(36).slice(2, 8);
+    return 'new-' + crypto.randomUUID();
 }
 
 function rowsKeyForMaster(masterId) {
