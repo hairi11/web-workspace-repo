109128e6b4650ba4f81f3a9d04cbc0f7aeaa5a16
hairi11 <mohd.khushairi@gmail.com>
2026-09-18 12:29:02 +0800

    Test ChoiceInput radio and select threshold

---
+++ common-js-web/test/choice-input.test.js
@@ -0,0 +1,19 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const ChoiceInput = require('../src/select/ChoiceInput');
+
+test('ChoiceInput uses radio for fewer than five options', function () {
+    assert.equal(ChoiceInput.resolveMode(0), ChoiceInput.Mode.RADIO);
+    assert.equal(ChoiceInput.resolveMode(1), ChoiceInput.Mode.RADIO);
+    assert.equal(ChoiceInput.resolveMode(4), ChoiceInput.Mode.RADIO);
+});
+
+test('ChoiceInput uses select for five or more options', function () {
+    assert.equal(ChoiceInput.resolveMode(5), ChoiceInput.Mode.SELECT);
+    assert.equal(ChoiceInput.resolveMode(6), ChoiceInput.Mode.SELECT);
+});
+
+test('ChoiceInput supports a custom threshold', function () {
+    assert.equal(ChoiceInput.resolveMode(2, 3), ChoiceInput.Mode.RADIO);
+    assert.equal(ChoiceInput.resolveMode(3, 3), ChoiceInput.Mode.SELECT);
+});
