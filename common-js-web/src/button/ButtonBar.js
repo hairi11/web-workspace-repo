02f79c87c088f03aaefc93347ebb9d3a19857c40
hairi11 <mohd.khushairi@gmail.com>
2026-09-18 10:39:16 +0800

    Add reusable ButtonBar component

---
+++ common-js-web/src/button/ButtonBar.js
@@ -0,0 +1,89 @@
+const Button = require('./Button');
+const Navigator = require('./Navigator');
+
+class ButtonBar {
+    constructor(target) {
+        this.target = target;
+        this.element = null;
+        this.primaryConfigs = [];
+        this.secondaryConfigs = [];
+        this.navigatorConfig = null;
+        this.buttons = [];
+        this.navigator = null;
+    }
+
+    primary(config) {
+        this.addConfigs(this.primaryConfigs, config, Button.Variant.PRIMARY);
+        return this;
+    }
+
+    secondary(config) {
+        this.addConfigs(this.secondaryConfigs, config, Button.Variant.SECONDARY);
+        return this;
+    }
+
+    navigation(config) {
+        this.navigatorConfig = config || null;
+        return this;
+    }
+
+    build() {
+        this.element = this.resolveElement(this.target);
+
+        if (!this.element) {
+            throw new Error('ButtonBar element not found.');
+        }
+
+        this.element.classList.add('button-bar');
+
+        this.buttons = this.secondaryConfigs
+            .concat(this.primaryConfigs)
+            .map((config) => new Button(config.target, config.options).build());
+
+        if (this.navigatorConfig) {
+            this.navigator = new Navigator(this.navigatorConfig).build();
+        }
+
+        return this;
+    }
+
+    destroy() {
+        this.buttons.forEach((button) => button.destroy());
+        this.buttons = [];
+
+        if (this.navigator) {
+            this.navigator.destroy();
+            this.navigator = null;
+        }
+
+        this.element = null;
+        return this;
+    }
+
+    addConfigs(destination, config, variant) {
+        var configs = Array.isArray(config) ? config : [config];
+
+        configs
+            .filter(Boolean)
+            .forEach((item) => {
+                if (!item.target) {
+                    throw new Error('Button target is required.');
+                }
+
+                destination.push({
+                    target: item.target,
+                    options: Object.assign({}, item, {variant: variant})
+                });
+            });
+    }
+
+    resolveElement(target) {
+        if (typeof target === 'string') {
+            return document.querySelector(target);
+        }
+
+        return target || null;
+    }
+}
+
+module.exports = ButtonBar;
