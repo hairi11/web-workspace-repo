713219461ac487113d60e6da321d67fc78727ca9
hairi11 <mohd.khushairi@gmail.com>
2026-09-18 11:05:02 +0800

    Type split ButtonDropdown action

--- common-js-web/index.d.ts
+++ common-js-web/index.d.ts
@@ -63,6 +63,7 @@ export interface ButtonDropdownOptions {
     menu: string | HTMLElement;
     variant?: 'primary' | 'secondary';
     hidden?: boolean;
+    action?: ButtonDropdownItemConfig;
     items: ButtonDropdownItemConfig[];
 }
 
